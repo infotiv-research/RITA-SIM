141cc5930448b1841133e07b77f3db07117f9efe
Marcus Olsson <marcus.olsson@infotiv.se>
2026-02-18 09:42:45 +0000

    fix: update documentation

--- .devcontainer/ros2/post_start.sh
+++ .devcontainer/ros2/post_start.sh
@@ -3,11 +3,7 @@ set -e
 
 cd /ros2_ws
 
-if [ ! -d "build" ] || [ -z "$(ls -A build)" ]; then
-  echo "🚧 No build/ folder found. Running colcon build..."
-  source /opt/ros/$ROS_DISTRO/setup.bash
-  colcon build --symlink-install
-  echo "✅ colcon build complete."
-else
-  echo "✅ build/ folder already exists. Skipping build."
-fi
+echo "Running colcon build on container start..."
+source /opt/ros/$ROS_DISTRO/setup.bash
+colcon build --symlink-install
+echo "colcon build complete."
