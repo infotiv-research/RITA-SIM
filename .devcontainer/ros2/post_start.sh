08563e1d214879b9caa6d2dc347010aa4304dcd9
Hamid Ebadi <hamid.ebadi@gmail.com>
2026-02-19 08:10:42 +0000

    fix: post container creation colcon error

--- .devcontainer/ros2/post_start.sh
+++ .devcontainer/ros2/post_start.sh
@@ -3,7 +3,11 @@ set -e
 
 cd /ros2_ws
 
-echo "Running colcon build on container start..."
-source /opt/ros/$ROS_DISTRO/setup.bash
-colcon build --symlink-install
-echo "colcon build complete."
+if [ ! -d "build" ] || [ -z "$(ls -A build)" ]; then
+  echo "🚧 No build/ folder found. Running colcon build..."
+  source /opt/ros/$ROS_DISTRO/setup.bash
+  colcon build --symlink-install
+  echo "✅ colcon build complete."
+else
+  echo "✅ build/ folder already exists. Skipping build."
+fi
